6b4af5bae8395004beb532998595e51202cb9985
evergreenempire83-sudo <evergreenempire83@gmail.com>
2025-11-11 12:31:11 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Service Worker for EverGreen Empire Portal
+const CACHE_NAME = 'evergreen-portal-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-384x384.png',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
