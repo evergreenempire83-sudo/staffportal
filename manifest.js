864f6fd312f78f683572b3ab78a0180ea37bf444
evergreenempire83-sudo <evergreenempire83@gmail.com>
2025-11-11 02:56:53 -0800

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+  "name": "EverGreen Empire Portal",
+  "short_name": "EverGreen Portal",
+  "description": "Staff Portal for EverGreen Empire - Enter the portal. Build the Empire.",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#0c1427",
+  "theme_color": "#00d09c",
+  "orientation": "portrait-primary",
+  "icons": [
+    {
+      "src": "icons/icon-384x384.png",
+      "sizes": "384x384",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
